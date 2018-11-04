2b24c52d0ee3d6436fb58e9b294858ca6840abb9
Marius Gheorghe <gmarius@gmail.com>
2018-11-05 00:19:02 +0200

    .netstandard project

--- MicroRuntime/StreamUtilities.cs
+++ MicroRuntime/StreamUtilities.cs
@@ -2,6 +2,9 @@ using System.IO;
 using System.Text;
 using System.Web;
 
+#if NETSTANDARD2_0
+using Microsoft.AspNetCore.Http;
+#endif
 namespace Microruntime
 {
 	public class StreamUtilities
@@ -21,12 +24,23 @@ namespace Microruntime
 			return ms;
 		}
 
+#if NETSTANDARD2_0
+		public byte[] GetBytesFromFormFile(IFormFile file)
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				file.CopyTo(ms);
+				ms.Position = 0;
+				return ms.ToArray();
+			}
+		}
+#else
 		public byte[] GetBytesFromHttpFileBase(HttpPostedFileBase file)
 		{
 			Stream stream = file.InputStream;
 			return ToBytes(stream);
 		}
-
+#endif
 		public byte[] ToBytes(Stream stream)
 		{
 			using (MemoryStream ms = new MemoryStream())
